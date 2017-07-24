93ea2ec260f2de3eb29009f821e07a40ea91f436
Dixon Dick <dixon1e@gmail.com>
2017-07-24 14:44:29 -0600

    remove extra cruft

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,9 @@
 dockerName=lsw17api
 port=8888
 logging=syslog
-#host_share=/home/cownby/api/app
-#container_share=/app
 
 docker stop $dockerName;
 docker rm $dockerName;
 docker rmi $dockerName;
 docker build --no-cache=true -t $dockerName .;
-#docker run -d --name $dockerName -p $port:80 --log-driver $logging -v ${host_share}:${container_share}  $dockerName
 docker run -d --name $dockerName -p $port:80 --log-driver $logging   $dockerName
